2e3bf2695c7a2f4a3f62f0902fd303584cac9a4a
ion <ayanrasulova13@gmail.com>
2026-09-24 19:06:40 -0400

    scale dragging

--- scripts/recipe.gd
+++ scripts/recipe.gd
@@ -4,6 +4,10 @@ var dragging := false
 var drag_offset := Vector2.ZERO
 
 
+# clamp boundaries 
+var min_boundary := Vector2(1275, 200)
+var max_boundary := Vector2(1980, 600)
+
 func _ready():
 	print("RECIPE SCRIPT IS RUNNING")
 
@@ -18,28 +22,36 @@ func _input(event):
 
 					dragging = true
 					drag_offset = global_position - get_global_mouse_position()
+					$Sprite2D.scale = Vector2(1.1, 1.1)
 
 					get_viewport().set_input_as_handled()
 
 			else:
-				if dragging:
-					print("RELEASED RECIPE")
-
 				dragging = false
+				$Sprite2D.scale = Vector2(1.0, 1.0)
 
 
 	elif event is InputEventMouseMotion:
 		if dragging:
-			var mouse_position = get_global_mouse_position()
+			var target_position = get_global_mouse_position() + drag_offset
 
-			global_position = mouse_position + drag_offset 
+			# clamp recipe position
+			global_position.x = clamp(
+				target_position.x,
+				min_boundary.x,
+				max_boundary.x
+			)
+
+			global_position.y = clamp(
+				target_position.y,
+				min_boundary.y,
+				max_boundary.y
+			)
+			
 
 
 func is_mouse_over_recipe() -> bool:
 	var sprite = $Sprite2D
-
 	var mouse_local = sprite.to_local(get_global_mouse_position())
 
-	var sprite_rect = sprite.get_rect()
-
-	return sprite_rect.has_point(mouse_local)
+	return sprite.get_rect().has_point(mouse_local)
