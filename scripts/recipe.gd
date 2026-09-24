57597c722a425f04adf5697dd894f69008a4f9fd
ion <ayanrasulova13@gmail.com>
2026-09-23 22:10:12 -0400

    .

---
+++ scripts/recipe.gd
@@ -0,0 +1,47 @@
+extends Area2D
+
+var dragging := false
+var drag_offset := Vector2.ZERO
+
+
+func _ready():
+	print("RECIPE SCRIPT IS RUNNING")
+
+
+func _input(event):
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT:
+
+			if event.pressed:
+				if is_mouse_over_recipe():
+					print("CLICKED RECIPE")
+
+					dragging = true
+					drag_offset = global_position - get_global_mouse_position()
+
+					get_viewport().set_input_as_handled()
+
+			else:
+				if dragging:
+					print("RELEASED RECIPE")
+
+				dragging = false
+
+
+	elif event is InputEventMouseMotion:
+		if dragging:
+			var mouse_position = get_global_mouse_position()
+
+			global_position = mouse_position + drag_offset
+
+
+func is_mouse_over_recipe() -> bool:
+	var sprite = $Sprite2D
+
+	# Convert the mouse position into the Sprite2D's local coordinates.
+	var mouse_local = sprite.to_local(get_global_mouse_position())
+
+	# Get the Sprite2D's actual texture rectangle.
+	var sprite_rect = sprite.get_rect()
+
+	return sprite_rect.has_point(mouse_local)
